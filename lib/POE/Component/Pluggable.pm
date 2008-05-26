03b376c600d85e7e21d9ddfeb6ac4413c01b6b7a
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2008-05-26 00:25:59 +0000

    Fix typo

--- lib/POE/Component/Pluggable.pm
+++ lib/POE/Component/Pluggable.pm
@@ -15,7 +15,8 @@ sub _pluggable_init {
   $self->{_pluggable_reg_prefix} = 'plugin_' unless $self->{_pluggable_reg_prefix};
   $self->{_pluggable_prefix} = 'pluggable_' unless $self->{_pluggable_prefix};
   if (ref $self->{_pluggable_types} eq 'ARRAY') {
-    $self->{_pluggable_types} = { map { ($_ => $_)  } $self->{_pluggable_types} };
+    $self->{_pluggable_types} = { map { ($_ => $_)  } @{ $self->{_pluggable_types} } };
+    print Dumper($self->{_pluggable_types});
   }
   elsif (ref $self->{_pluggable_types} ne 'HASH') {
     $self->{_pluggable_types} = {};
